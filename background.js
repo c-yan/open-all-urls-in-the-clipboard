7133faef6e0c902ee35c9d9f6d341bd5210aea54
c-yan <c-yan@users.noreply.github.com>
2018-06-09 09:11:17 +0000

    Removed unnecessary paren

--- background.js
+++ background.js
@@ -11,7 +11,7 @@
 
         let urls = [];
         for (const l of text.split('\n')) {
-            const m = l.match(/(https?:\/\/[^>\s]+)/g);
+            const m = l.match(/https?:\/\/[^>\s]+/g);
             if (m == null) continue;
 
             for (const u of m) urls.push(u);
